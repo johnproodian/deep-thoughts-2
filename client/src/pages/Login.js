6c93239a163c189d7d3ae2f84c5e313d578313c9
Jotas <jproodian721@gmail.com>
2022-06-29 14:30:44 -0500

    create and implement authentication functionality for login and signup with authservice class

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [ login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -11,18 +17,25 @@ const Login = (props) => {
       ...formState,
       [name]: value,
     });
-  };
+  }
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+    // submit form
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.log(e);
+    }
+
   };
+
+  
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +66,8 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
